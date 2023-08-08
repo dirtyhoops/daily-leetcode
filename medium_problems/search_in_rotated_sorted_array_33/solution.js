8e81df4a9cae7498628ad9a3b10897e8c1eb273a
Daryll <daryllosis27@gmail.com>
2023-08-08 14:32:57 -0700

    solved problemd #33. Search in Rotated Sorted Array

---
+++ medium_problems/search_in_rotated_sorted_array_33/solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    // Check if mid is target
+    if (nums[mid] === target) return mid;
+
+    // Check if left side is sorted
+    if (nums[start] <= nums[mid]) {
+      // Check if the target is in the left side
+      if (nums[start] <= target && target <= nums[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+
+  // If target is not found return -1
+  return -1;
+};
