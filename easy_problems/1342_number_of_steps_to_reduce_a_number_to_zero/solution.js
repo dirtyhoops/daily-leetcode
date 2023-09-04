5219fe71a27bc93ca276dd6d90e6f919bb1cfc91
Daryll <daryllosis27@gmail.com>
2023-09-04 13:19:22 -0700

    added a recursive solution

--- easy_problems/1342_number_of_steps_to_reduce_a_number_to_zero/solution.js
+++ easy_problems/1342_number_of_steps_to_reduce_a_number_to_zero/solution.js
@@ -2,6 +2,13 @@
  * @param {number} num
  * @return {number}
  */
+
+// Solution 2. Recursive Solution
+var numberOfSteps = function (num) {
+  return num === 0 ? 0 : 1 + numberOfSteps(num % 2 === 0 ? num / 2 : num - 1);
+};
+
+// Solution 1. Iterative Solution
 var numberOfSteps = function (num) {
   let steps = 0;
 
