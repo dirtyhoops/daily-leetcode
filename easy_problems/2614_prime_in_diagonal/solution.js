6174e69d1b8a3092938a53c751a78a86a9365751
Daryll <daryllosis27@gmail.com>
2023-09-06 19:32:05 -0700

    updated solution

--- easy_problems/2614_prime_in_diagonal/solution.js
+++ easy_problems/2614_prime_in_diagonal/solution.js
@@ -2,19 +2,29 @@
  * @param {number[][]} nums
  * @return {number}
  */
+
+// Cleaner Solution compare to the one below
 var diagonalPrime = function (nums) {
   let maxPrime = 0;
+  let n = nums.length;
 
   // Basically checks if the numbers that is in diagonals of is prime and then compare it to the maxPrime
-  for (let i = 0; i < nums[0].length; i++) {
-    if (isPrime(nums[i][i])) maxPrime = Math.max(maxPrime, nums[i][i]);
-    if (isPrime(nums[i][nums[0].length - 1 - i]))
-      maxPrime = Math.max(maxPrime, nums[i][nums[0].length - 1 - i]);
+  for (let i = 0; i < n; i++) {
+    const left = nums[i][i];
+    if (left > maxPrime && isPrime(left)) {
+      maxPrime = left;
+    }
+
+    const right = nums[i][n - 1 - i];
+    if (right > maxPrime && isPrime(right)) {
+      maxPrime = right;
+    }
   }
 
   return maxPrime;
 };
 
+// Faster solution than brute force
 function isPrime(num) {
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) return false;
@@ -22,6 +32,20 @@ function isPrime(num) {
 
   return num > 1;
 }
+
+// // Old Solution
+// var diagonalPrime = function (nums) {
+//   let maxPrime = 0;
+
+//   // Basically checks if the numbers that is in diagonals of is prime and then compare it to the maxPrime
+//   for (let i = 0; i < nums[0].length; i++) {
+//     if (isPrime(nums[i][i])) maxPrime = Math.max(maxPrime, nums[i][i]);
+//     if (isPrime(nums[i][nums[0].length - 1 - i]))
+//       maxPrime = Math.max(maxPrime, nums[i][nums[0].length - 1 - i]);
+//   }
+
+//   return maxPrime;
+// };
 
 // Brute Force way of checking if a number is prime
 // function isPrime(num) {
