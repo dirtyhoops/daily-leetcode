e102c30f2e8724ac778a8279816887f1b13c9eb5
Daryll <daryllosis27@gmail.com>
2023-09-11 13:39:08 -0700

    solved problem #58. Length of Last Word. Included 3 solutions: converting to string, one-liner of the first solution, and then the long way without using built-in functions.

---
+++ easy_problems/58_length_of_last_word/README.md
@@ -0,0 +1,36 @@
+# 58. Length of Last Word
+
+- Link: https://leetcode.com/problems/length-of-last-word/
+- Submission Detail: https://leetcode.com/submissions/detail/1046870375/
+- Date Solved: Sept. 11, 2023
+
+# Description
+
+Given a string `s` consisting of words and spaces, return the length of the **last** word in the string.
+
+A **word** is a maximal substring consisting of non-space characters only.
+
+**Example 1**:
+
+```
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+```
+
+**Example 2**:
+
+```
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+```
+
+**Example 3**:
+
+```
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+```
---
+++ easy_problems/58_length_of_last_word/solution.ts
@@ -0,0 +1,34 @@
+// Solution #3: Without built-in functions
+function lengthOfLastWord(s: string): number {
+  let countStarted = false;
+  let len = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] !== ' ' && !countStarted) countStarted = true;
+    if (s[i] === ' ' && countStarted) break;
+    if (countStarted) ++len;
+  }
+
+  return len;
+}
+
+// Solution #2: Basically same idea as solution #1 but one liner
+function lengthOfLastWord(s: string): number {
+  return s
+    .split(' ')
+    .reverse()
+    .find(word => word !== '').length;
+}
+
+// Solution #1: Split strings into array and return the first non empty string starting from last index
+function lengthOfLastWord(s: string): number {
+  const arr = s.split(' ');
+  let i = arr.length - 1;
+
+  while (i >= 0) {
+    if (arr[i] !== '') break;
+    i--;
+  }
+
+  return arr[i].length;
+}
