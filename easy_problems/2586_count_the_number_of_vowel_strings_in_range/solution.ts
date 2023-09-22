21acb23abee16e2658cdb66ac8d8678ee44b3e36
Daryll <daryllosis27@gmail.com>
2023-09-22 11:43:05 -0700

    solved problem #2586

---
+++ easy_problems/2586_count_the_number_of_vowel_strings_in_range/solution.ts
@@ -0,0 +1,12 @@
+function vowelStrings(words: string[], left: number, right: number): number {
+  const set = new Set<string>(['a', 'e', 'i', 'o', 'u']);
+  let vowelStringCount = 0;
+
+  for (let i = left; i <= right; i++) {
+    if (set.has(words[i][0]) && set.has(words[i].at(-1))) {
+      vowelStringCount++;
+    }
+  }
+
+  return vowelStringCount;
+}
