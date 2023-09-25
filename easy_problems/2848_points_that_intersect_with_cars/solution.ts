c25769b30821d534740ee790527a0a7a85fa279a
Daryll <daryllosis27@gmail.com>
2023-09-25 09:17:16 -0700

    solved problem #2848

---
+++ easy_problems/2848_points_that_intersect_with_cars/solution.ts
@@ -0,0 +1,11 @@
+function numberOfPoints(nums: number[][]): number {
+  const set = new Set();
+
+  for (const [start, end] of nums) {
+    for (let i = start; i <= end; i++) {
+      set.add(i);
+    }
+  }
+
+  return set.size;
+}
